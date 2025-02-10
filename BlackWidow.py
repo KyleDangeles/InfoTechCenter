6391e5ee0cbc36c7a842347273b854171c700342
KyleDangeles <Kaledelosan@gmail.com>
2025-02-10 14:10:02 -0500

    Updated Weather Branch with Comments

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,36 +1,50 @@
+# Print a decorative header
 print("\n*************************************\n")
-
 print("Weather Branch - Developer: Mr. Lange\n")
 
-#Import Libraries Here!
-import random
-from time import sleep
+# Import necessary libraries
+import random  # Used for generating random weather conditions
+from time import sleep  # Sleep function can be used if delays are needed
 
-#Weather Function to determine the weather
+# Weather Function to determine the weather
 def weather():
+    # List of possible weather conditions
     weatherForecastList = ["snowy", "blizzard", "icy", "rainy", "windy", "sunny"]
+    
+    # Randomly select a weather condition from the list
     weatherCondition = random.choice(weatherForecastList)
+    
+    # Return the selected weather condition
     return weatherCondition
 
+# Call the weather function to get the weather alert
 weatherAlert = weather()
 
+# Vehicle Response System function to determine what action to take based on the weather
 def vehicleResponseSystem():
+    # Check if the weather is snowy and update the alarm accordingly
     if weatherAlert == "snowy":
         print("\nThe National Weather Service has updated our alarm by 30 minutes because"
-        " it is", weatherAlert, "outside.")
+              " it is", weatherAlert, "outside.")
+    # Check if the weather is a blizzard and update the alarm accordingly
     elif weatherAlert == "blizzard":
         print("\nThe National Weather Service has updated our alarm by 60 minutes because"
-        " it is a", weatherAlert, "outside!")
+              " it is a", weatherAlert, "outside!")
+    # Check if the weather is icy and update the alarm accordingly
     elif weatherAlert == "icy":
         print("\nThe National Weather Service has updated our alarm by 90 minutes because"
-        " it is", weatherAlert, "outside.")
+              " it is", weatherAlert, "outside.")
+    # Check if the weather is rainy and update the alarm accordingly
     elif weatherAlert == "rainy":
         print("\nThe National Weather Service has updated our alarm by 10 minutes because"
-        " it is", weatherAlert, "outside.")
+              " it is", weatherAlert, "outside.")
+    # Check if the weather is windy and update the alarm accordingly
     elif weatherAlert == "windy":
         print("\nThe National Weather Service has updated our alarm by 5 minutes because"
-        " it is", weatherAlert, "outside.")
+              " it is", weatherAlert, "outside.")
+    # If the weather is sunny, print a safety message
     else:
-        print("\nThe National Weather Service is calling for", weatherAlert, "skys outside, drive safe!")
-        
+        print("\nThe National Weather Service is calling for", weatherAlert, "skies outside, drive safe!")
+
+# Call the vehicle response system to update the alarm based on the weather
 vehicleResponseSystem()
